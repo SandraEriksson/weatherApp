f43f0db4bbd4615bb5975aee8ab942c3c398197a
Sandra Eriksson <sandraerikss0n@hotmail.com>
2023-05-29 12:58:23 +0200

    fetch api

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ let desc = document.querySelector('.weather-description');
 let pressure = document.querySelector('.pressure');
 let humidity = document.querySelector('.humidity');
 let wind = document.querySelector('.wind');
+
+
+fetch('http://api.openweathermap.org/data/2.5/weather?id=2664454&units=metric&appid=ff8026ab97b7f4d14aa6b8af9b95210b') // hämta ifrån url
+.then(res => { 
+    let json = res.json();
+    return json;
+})
