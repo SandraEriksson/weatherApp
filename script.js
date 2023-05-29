b01a13ad8676a903ebb7d69b69c496925a01e490
Sandra Eriksson <sandraerikss0n@hotmail.com>
2023-05-29 12:57:05 +0200

    javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+let city = document.querySelector('#city');
+let temp = document.querySelector('.temp');
+let desc = document.querySelector('.weather-description');
+let pressure = document.querySelector('.pressure');
+let humidity = document.querySelector('.humidity');
+let wind = document.querySelector('.wind');
